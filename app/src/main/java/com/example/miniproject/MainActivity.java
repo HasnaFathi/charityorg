aa51820a0cb2177f4fb1742900111e0aba6d7b60
aaishaM <aaishamauroof99@gmail.com>
2021-09-30 20:06:15 +0530

    final integrated project

--- app/src/main/java/com/example/miniproject/MainActivity.java
+++ app/src/main/java/com/example/miniproject/MainActivity.java
@@ -1,99 +1,48 @@
 package com.example.miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.SearchView;
-
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recview;
-    myAdapter adapter;
-    FloatingActionButton fb;
+    Button btnproject,btnevent,btnOrg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       recview=(RecyclerView) findViewById(R.id.recview);
-       recview.setLayoutManager(new LinearLayoutManager(this));
+        btnproject=(Button)findViewById(R.id.btnproject);
+        btnevent=(Button)findViewById(R.id.btnevent);
+        btnOrg=(Button)findViewById(R.id.btnOrg);
 
-        FirebaseRecyclerOptions<Event> options =
-                new FirebaseRecyclerOptions.Builder<Event>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Event"), Event.class)
-                        .build();
-        adapter = new myAdapter(options);
-        recview.setAdapter(adapter);
 
-        fb=(FloatingActionButton) findViewById(R.id.fadd);
-        fb.setOnClickListener(new View.OnClickListener() {
+        btnproject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,addEvent.class);
-                startActivity(intent);
+                startActivity(new Intent(MainActivity.this,ProductLandingActivity.class));
+                finish();
             }
         });
 
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        adapter.startListening();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        adapter.stopListening();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.searchmenu,menu);
-        MenuItem item = menu.findItem(R.id.search);
-
-        SearchView searchView = (SearchView) item.getActionView();
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        btnevent.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onQueryTextSubmit(String s) {
-                processsearch(s);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                processsearch(s);
-                return false;
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,EventMainActivity.class));
+                finish();
             }
         });
-        return super.onCreateOptionsMenu(menu);
-    }
 
-    private void processsearch(String s) {
-
-        FirebaseRecyclerOptions<Event> options =
-                new FirebaseRecyclerOptions.Builder<Event>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Event").orderByChild("name").startAt(s).endAt(s+"\uf8ff"),Event.class)
-                        .build();
-        adapter = new myAdapter(options);
-        adapter.startListening();
-        recview.setAdapter(adapter);
-
-
+        btnOrg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Org_MainActivity.class));
+                finish();
+            }
+        });
     }
 }
